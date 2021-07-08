f39ba54e3d9912afd96c05fed550ba32390c6ded
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-07-08 17:15:37 -0500

    rick

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+// https://rickandmortyapi.com/api/character/
 
 function App() {
+
+    const [personajes, setPersonajes]=useState([])  
+
+useEffect(()=>{
+      axios.get('https://rickandmortyapi.com/api/character/').then(res=>{
+setPersonajes(res.data.results)
+//console.log(res.data)
+      }).catch(error=>console.log(error))
+
+},[])
+
+console.log(personajes)
+
   return (
+      //"row row-cols-1 row-cols-md-3 g4"  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+   <h1 > Api Rick and Morty </h1>
+                            
+<div className="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+
+{  personajes.map(per=>(
+<div className="col" key={per.id}>      
+<div className="card" style={{width: "13rem",height:"18rem"}}>
+  <img src={`${per.image}`} className="card-img-top" style={{width:"80%",height:"80%"}}
+  class="card-img-top"/>
+  <div className="card-body">
+    <p className="card-text">{per.name}</p>
     </div>
+  </div>
+</div>
+
+
+))
+
+
+
+
+
+
+
+}
+</div>
+ </div>
   );
 }
 
